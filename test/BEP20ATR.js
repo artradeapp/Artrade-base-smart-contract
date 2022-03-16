ffb7ff6c4f332b2e9291821287545c7614d9c600
Artrade <94073854+artradeapp@users.noreply.github.com>
2022-03-16 14:32:28 +0100

    Update BEP20ATR.js
    update test

--- test/BEP20ATR.js
+++ test/BEP20ATR.js
@@ -1,11 +1,14 @@
 const BEP20 = artifacts.require("BEP20");
 const day = 24 * 60 * 60;
+const month = 31 * day ;
 const { BN, time } = require('@openzeppelin/test-helpers');
-
+let icoNumber = 0;
 contract('BEP20', (accounts) => {
 
   beforeEach(async function() {
     const bep = await BEP20.deployed();
+    const endOfICO = await bep.getEndOfICO();
+    icoNumber = endOfICO.toNumber();
     //await time.increaseTo(1616581471);
   });
   it('Test1 : The Balalance of the owner should be 0', async () => {
@@ -24,28 +27,25 @@ contract('BEP20', (accounts) => {
 
 it('Test1  test  mint  with  respect of Cliff  and Vesting : ', async () => {
   const bep = await BEP20.deployed();
-  //  1 S from End of ICO
-  await time.increaseTo(1644879601);
-  await bep.mint(1);
+
   // 2 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601); //  15 avr
+  await time.increaseTo(icoNumber + 2 * month); //  15 avr
   await bep.mint(2);
 
   // 3 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 2674800); // 15 mai 2022 00:00:01
+  await time.increaseTo(icoNumber + 3 * month ); // 15 mai 2022 00:00:01
   await bep.mint(2);
   await bep.mint(3);
 
   // 4 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 2*2674800); //== 1655157601  14 juin 2022 00:00:01 GMT+02:00
+  await time.increaseTo(icoNumber + 4 * month); //== 1655157601  14 juin 2022 00:00:01 GMT+02:00
   await bep.mint(2);
   await bep.mint(3);
-  await bep.mint(4);
 
 
 
   // 5 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 3*2674800);
+  await time.increaseTo(icoNumber + 5 * month);
   await bep.mint(2);
   await bep.mint(3);
   await bep.mint(4);
@@ -53,17 +53,16 @@ it('Test1  test  mint  with  respect of Cliff  and Vesting : ', async () => {
 
 
   // 6 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 4*2674800);
+  await time.increaseTo(icoNumber + 6 * month);
   await bep.mint(3);
   await bep.mint(4);
-  await bep.mint(6);
 
 
 
 
 
   // 7 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 5*2674800);
+  await time.increaseTo(icoNumber + 7 * month);
   await bep.mint(3);
   await bep.mint(4);
   await bep.mint(6);
@@ -71,7 +70,7 @@ it('Test1  test  mint  with  respect of Cliff  and Vesting : ', async () => {
 
 
   // 8 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 6*2674800);
+  await time.increaseTo(icoNumber + 8 * month);
   await bep.mint(3);
   await bep.mint(4);
   await bep.mint(6);
@@ -79,89 +78,92 @@ it('Test1  test  mint  with  respect of Cliff  and Vesting : ', async () => {
 
 
   // 9 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 7*2674800);
+  await time.increaseTo(icoNumber + 9 * month);
   await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 10 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 8*2674800);
+  await time.increaseTo(icoNumber + 10 * month);
   await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 11 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 9*2674800);
+  await time.increaseTo(icoNumber + 11 * month);
   await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 12 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 10*2674800);
+  await time.increaseTo(icoNumber + 12 * month);
   await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 13 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 11*2674800);
+  await time.increaseTo(icoNumber + 13 * month);
   await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 14 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 12*2674800);
+  await time.increaseTo(icoNumber + 14 * month);
   await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 15 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 13*2674800);
+  await time.increaseTo(icoNumber + 15 * month);
   await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 16 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 14*2674800);
+  await time.increaseTo(icoNumber + 16 * month);
+  await bep.mint(4);
   await bep.mint(6);
 
 
 
   // 17 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 15*2674800);
+  await time.increaseTo(icoNumber + 17 * month);
   await bep.mint(6);
 
   // 18 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 16*2674800);
+  await time.increaseTo(icoNumber + 18 * month);
   await bep.mint(6);
 
   // 19 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 17*2674800);
+  await time.increaseTo(icoNumber + 19 * month);
   await bep.mint(6);
 
   // 20 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 18*2674800);
+  await time.increaseTo(icoNumber + 20 * month);
   await bep.mint(6);
 
   // 21 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 19*2674800);
+  await time.increaseTo(icoNumber + 21 * month);
   await bep.mint(6);
 
   // 22 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 20*2674800);
+  await time.increaseTo(icoNumber + 22 * month);
   await bep.mint(6);
 
   // 23 M  and 1 S from End of ICO
-  await time.increaseTo(1649973601 + 21*2674800);
+  await time.increaseTo(icoNumber + 23 * month);
   await bep.mint(6);
 
-
+  // 23 M  and 1 S from End of ICO
+  await time.increaseTo(icoNumber + 24 * month);
+  await bep.mint(6);
 
 
   const balanceAss  = await bep.balanceOf(accounts[1]);
@@ -170,25 +172,16 @@ it('Test1  test  mint  with  respect of Cliff  and Vesting : ', async () => {
   const balancePriv  = await bep.balanceOf(accounts[4]);
   const balanceMark = await bep.balanceOf(accounts[5]);
   const balanceICOR1 = await bep.balanceOf(accounts[6]);
-  const balanceICOR2 = await bep.balanceOf(accounts[7]);
-  const balanceICOR3 = await bep.balanceOf(accounts[8]);
-  const balanceReserve = await bep.balanceOf(accounts[9]);
 
 
 
-  console.log("Balance of Reserve at the End of ICO            : " +balanceReserve.valueOf());
+
   console.log("Balance of Associates at the End of ICO         : " +balanceAss.valueOf());
   console.log("Balance of Teams &  advisors at the End of ICO  : "  +balanceTeam.valueOf());
   console.log("Balance of Seed supporters at the End of ICO    : " +balanceSeed.valueOf());
   console.log("Balance of Private sale at the End of ICO       : " +balancePriv.valueOf());
   console.log("Balance of Marketing, airdrop at the End of ICO : " +balanceMark.valueOf());
   console.log("Balance of ICO R1 at the End of ICO             : " +balanceICOR1.valueOf());
-  console.log("Balance of ICO R2 at the End of ICO             : " +balanceICOR2.valueOf());
-  console.log("Balance of ICO R3 at the End of ICO             : " +balanceICOR3.valueOf());
 
 });
-
-
-
-
 });
